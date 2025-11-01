65a9ef0f8cd29e8269fa53d4c7d25c835f933c36
Ellejun Sampaga <ellejunsampaga@gmail.com>
2025-11-01 20:50:00 +0800

    Auto commit: 11/1/2025, 8:50:00 PM

--- src/pages/SideNavBar.js
+++ src/pages/SideNavBar.js
@@ -1,29 +1,23 @@
-import React, { useState } from "react";
-import {
-  Box,
-  Typography,
-  Drawer,
-  IconButton,
-  useMediaQuery,
-} from "@mui/material";
+import React from "react";
+import { Box, Typography } from "@mui/material";
 import { NavLink } from "react-router-dom";
 import { navConfig } from "./navConfig";
 import theme from "./../theme";
 import { useRole } from "../context/RoleContext";
-import MenuIcon from "@mui/icons-material/Menu";
 
 const SideNavBar = () => {
   const { role } = useRole();
   const navItems = navConfig[role] || [];
-  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
-  const [mobileOpen, setMobileOpen] = useState(false);
 
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
-
-  const drawerContent = (
-    <Box sx={{ width: 240, display: "flex", flexDirection: "column", p: 2 }}>
+  return (
+    <Box
+      sx={{
+        borderRight: "1px solid gray",
+        width: "20%",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
       {navItems.map((item, index) => (
         <NavLink
           key={index}
@@ -40,7 +34,6 @@ const SideNavBar = () => {
                 mt: 1,
                 cursor: "pointer",
                 height: 40,
-                border: "1px solid",
                 p: 2,
                 borderRadius: 2,
                 bgcolor: isActive
@@ -56,7 +49,10 @@ const SideNavBar = () => {
             >
               {item.icon}
               <Typography
-                sx={{ fontWeight: isActive ? 600 : 400, color: "inherit" }}
+                sx={{
+                  fontWeight: isActive ? 600 : 400,
+                  color: "inherit",
+                }}
               >
                 {item.label}
               </Typography>
@@ -65,45 +61,6 @@ const SideNavBar = () => {
         </NavLink>
       ))}
     </Box>
-  );
-
-  return (
-    <>
-      {isMobile ? (
-        <>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ m: 1 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Drawer
-            variant="temporary"
-            open={mobileOpen}
-            onClose={handleDrawerToggle}
-            ModalProps={{ keepMounted: true }} // Better open performance on mobile
-          >
-            {drawerContent}
-          </Drawer>
-        </>
-      ) : (
-        <Box
-          sx={{
-            borderRight: "1px solid gray",
-            width: "20%",
-            minWidth: 200,
-            display: "flex",
-            flexDirection: "column",
-            p: 1,
-          }}
-        >
-          {drawerContent}
-        </Box>
-      )}
-    </>
   );
 };
 
